d6fd2ddae25f663ef0637a860f8513209c844994
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-10-20 12:59:58 -0400

    refactor: add CameraPreview composable

---
+++ app/src/main/java/com/example/lookup/ui/components/CameraPreview.kt
@@ -0,0 +1,31 @@
+package com.example.lookup.ui.components
+
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+
+/**
+ * A camera preview composable that uses [PreviewView] under the hood.
+ *
+ * @param controller The [LifecycleCameraController] instance that controls the camera.
+ * @param modifier The [Modifier] to be applied to this composable.
+ */
+@Composable
+fun CameraPreview(
+    controller: LifecycleCameraController,
+    modifier: Modifier = Modifier
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    AndroidView(
+        factory = { context ->
+            PreviewView(context).apply {
+                this.controller = controller
+                controller.bindToLifecycle(lifecycleOwner)
+            }
+        },
+        modifier = modifier
+    )
+}
