f4d3486f0f0db7cdce2f4e1b3f717b3f235476c5
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-10-22 20:05:35 -0400

    refactor: add HomeScreenUiState data class

---
+++ app/src/main/java/com/example/lookup/ui/home/HomeScreenUiState.kt
@@ -0,0 +1,12 @@
+package com.example.lookup.ui.home
+
+/**
+ * A data class that represents the current UI state of the [HomeScreen].
+ *  @param identifiedLocation The location identified by the app. A null value indicates
+ *  that the app hasn't identified any locations yet.
+ *  @param isAnalyzing Indicates whether the current camera stream is being analyzed.
+ */
+data class HomeScreenUiState(
+    val identifiedLocation: IdentifiedLocation? = null,
+    val isAnalyzing: Boolean = false
+)
