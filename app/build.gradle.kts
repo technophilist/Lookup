ba7c760f7247c8ffa4449131941e9ec80a3cef1d
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-10-29 18:00:19 -0400

    refactpr: add retrofit moshi converter dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,5 +100,5 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
     kapt("com.squareup.moshi:moshi-kotlin-codegen:1.14.0")
-
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
 }
