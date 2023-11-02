1474e4b4d658a240997672aa388139b638f27eab
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-11-02 08:58:20 -0400

    refactor: add LookupDestinations sealed class

---
+++ app/src/main/java/com/example/lookup/ui/navigation/LookupDestinations.kt
@@ -0,0 +1,12 @@
+package com.example.lookup.ui.navigation
+
+/**
+ * A sealed class hierarchy that contains the different destinations of the app.
+ * @param route The associated route string of each destination.
+ */
+sealed class LookupDestinations(val route: String) {
+    /**
+     * An object representing the home screen.
+     */
+    object HomeScreen : LookupDestinations("lookup_home_screen")
+}
