cb05bcae7c5cb9816c131bb7e91976586c6f83ef
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-11-02 08:34:44 -0400

    refactor: add ClassifierConfiguration data class in LandmarksClassifier interface

--- app/src/main/java/com/example/lookup/data/local/classifiers/LandmarksClassifier.kt
+++ app/src/main/java/com/example/lookup/data/local/classifiers/LandmarksClassifier.kt
@@ -6,17 +6,6 @@ import android.graphics.Bitmap
  * An image based landmarks classifier.
  */
 interface LandmarksClassifier {
-    /**
-     * Data class representing a classified landmark
-     * @property name The name of the landmark
-     * @property score The confidence score of the classification.
-     */
-    data class LandmarkClassification(val name: String, val score: Float)
-
-    /**
-     * Enum representing the possible rotations of an image.
-     */
-    enum class Rotation { ROTATION_0, ROTATION_90, ROTATION_180, ROTATION_270 }
 
     /**
      * Classifies the landmark in the [bitmap], taking into consideration the [rotation]
@@ -27,4 +16,30 @@ interface LandmarksClassifier {
         bitmap: Bitmap,
         rotation: Rotation
     ): Result<List<LandmarkClassification>>
+
+    /**
+     * Data class representing a classified landmark
+     * @property name The name of the landmark
+     * @property score The confidence score of the classification.
+     */
+    data class LandmarkClassification(val name: String, val score: Float)
+
+    /**
+     * A data class that holds the configuration parameters for a concrete implementation of
+     * [LandmarksClassifier].
+     *
+     * @property classificationInclusionThreshold The minimum confidence score required for a classification
+     * to be included in the results. Default value is 0.5f.
+     * @property maxResults The maximum number of classifications to return. Default value is 1.
+     */
+    data class ClassifierConfiguration(
+        val classificationInclusionThreshold: Float = 0.5f,
+        val maxResults: Int = 1
+    )
+
+    /**
+     * Enum representing the possible rotations of an image.
+     */
+    enum class Rotation { ROTATION_0, ROTATION_90, ROTATION_180, ROTATION_270 }
+
 }
