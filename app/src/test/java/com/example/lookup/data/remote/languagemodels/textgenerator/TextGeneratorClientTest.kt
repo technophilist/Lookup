cb190f532fac4db728cc42c05d85c6cf3faafb99
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-11-04 13:19:12 -0400

    refactor: add additional test in TextGeneratorClientTest

--- app/src/test/java/com/example/lookup/data/remote/languagemodels/textgenerator/TextGeneratorClientTest.kt
+++ app/src/test/java/com/example/lookup/data/remote/languagemodels/textgenerator/TextGeneratorClientTest.kt
@@ -2,6 +2,7 @@ package com.example.lookup.data.remote.languagemodels.textgenerator
 
 import com.example.lookup.data.remote.languagemodels.textgenerator.models.MessageDTO
 import com.example.lookup.data.remote.languagemodels.textgenerator.models.TextGenerationPromptBody
+import com.example.lookup.data.remote.languagemodels.textgenerator.models.buildTextGenerationPromptBody
 import com.example.lookup.di.NetworkModule
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
@@ -32,6 +33,29 @@ class TextGeneratorClientTest {
             val generatedTextResponse =
                 textGeneratorClient.generateTextForPrompt(textGenerationPromptBody)
             println(generatedTextResponse.body())
+        }
+
+    @Test
+    fun `Given an API response of possible questions, the result must be successfully parsed into valid strings`() =
+        runBlocking {
+            val systemPrompt = """
+                List out 5 very short questions that a traveller might ask a guide about this place.
+            """.trimIndent()
+            val textGenerationPromptBody = buildTextGenerationPromptBody(
+                systemPrompt = systemPrompt,
+                userPrompt = "Eiffel Tower",
+                maxResponseTokens = 100
+            )
+            val generatedTextResponse =
+                textGeneratorClient.generateTextForPrompt(textGenerationPromptBody)
+            val questions =
+                generatedTextResponse.body()!!.generatedResponses.first().message.content
+            val questionsList = questions.lines().map {
+                it.replace(regex = Regex("[0-9]\\.\\s"), replacement = "")
+            }
+            // all numbers must be removed and the strings must only contain the question.
+            val regex = Regex(".*")
+            assert(questionsList.all(regex::matches))
         }
 
 }
