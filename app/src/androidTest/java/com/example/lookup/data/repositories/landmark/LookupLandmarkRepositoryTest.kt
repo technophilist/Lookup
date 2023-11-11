c7d4f94ebd7ec466ec92e953c1ba3687d55b5998
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-11-10 19:48:58 -0500

    test: add cache test in LookupLandmarkRepository

--- app/src/androidTest/java/com/example/lookup/data/repositories/landmark/LookupLandmarkRepositoryTest.kt
+++ app/src/androidTest/java/com/example/lookup/data/repositories/landmark/LookupLandmarkRepositoryTest.kt
@@ -6,6 +6,8 @@ import androidx.test.platform.app.InstrumentationRegistry
 import coil.imageLoader
 import coil.request.ImageRequest
 import coil.request.SuccessResult
+import com.example.lookup.data.local.cache.landmarks.RecognizedLandmarkEntity
+import com.example.lookup.data.local.cache.landmarks.RecognizedLandmarksDao
 import com.example.lookup.data.local.classifiers.LandmarksClassifier
 import com.example.lookup.data.local.classifiers.TFLiteLandmarkClassifier
 import com.example.lookup.di.NetworkModule
@@ -18,6 +20,7 @@ class LookupLandmarkRepositoryTest {
 
     private val context = InstrumentationRegistry.getInstrumentation().targetContext
     private val testDispatcher = StandardTestDispatcher()
+    private val testRecognizedLandmarkDao = TestRecognizedLandmarksDao()
     private val classifier = TFLiteLandmarkClassifier(
         context = context,
         ioDispatcher = testDispatcher,
@@ -25,7 +28,8 @@ class LookupLandmarkRepositoryTest {
     )
     private val landmarkRecognitionRepository = LookupLandmarkRepository(
         landmarksClassifier = classifier,
-        textGeneratorClient = NetworkModule.provideTextGeneratorClient()
+        textGeneratorClient = NetworkModule.provideTextGeneratorClient(),
+        recognizedLandmarksDao = testRecognizedLandmarkDao
     )
 
     @Test
@@ -74,5 +78,39 @@ class LookupLandmarkRepositoryTest {
             println(questionsList)
         }
 
+    @Test
+    fun cacheTest_afterASuccessfulFetch_resultMustBeCached() =
+        runTest(context = testDispatcher, timeout = 30.seconds) {
+            val goldenGateBridgeUrl =
+                "https://fastly.picsum.photos/id/392/5000/3333.jpg?hmac=vCaGuB6rQAiaofdQHatQL4DHgkyR2l-Ms9GWAL63CBQ"
+            val request = ImageRequest.Builder(context)
+                .data(goldenGateBridgeUrl)
+                .build()
 
+            val drawable = (context.imageLoader.execute(request) as SuccessResult).drawable
+            val bitmap = (drawable as BitmapDrawable).bitmap
+            val result = landmarkRecognitionRepository.getNameAndDescriptionOfLandmark(
+                bitmap,
+                Surface.ROTATION_90
+            )
+            assert(result.isSuccess)
+            // after a landmark is recognized, it must be saved in the cache.
+            val (name, _) = result.getOrThrow()
+            assert(
+                testRecognizedLandmarkDao.getRecognizedLandmarkEntityWithName(name) != null
+            )
+        }
+
+
+}
+
+private class TestRecognizedLandmarksDao : RecognizedLandmarksDao {
+    private val savedLandmarks = mutableListOf<RecognizedLandmarkEntity>()
+    override suspend fun insertRecognizedLandmark(recognizedLandmarkEntity: RecognizedLandmarkEntity) {
+        savedLandmarks.add(recognizedLandmarkEntity)
+    }
+
+    override suspend fun getRecognizedLandmarkEntityWithName(name: String): RecognizedLandmarkEntity? {
+        return savedLandmarks.firstOrNull { it.name == name }
+    }
 }
