c09f1e5708537cabd242d6e46cc2d2c66e5c376b
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-11-10 19:14:41 -0500

    refactor: add kotlinx-test coroutines dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -122,4 +122,7 @@ dependencies {
     // To use Kotlin annotation processing tool (kapt)
     kapt("androidx.room:room-compiler:2.6.0")
     implementation("androidx.room:room-ktx:2.6.0")
+
+    // coroutines test
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 }
