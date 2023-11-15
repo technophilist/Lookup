a9529dc37f16b38303c33707b608effbe3449ffd
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-11-15 08:53:22 -0500

    refactor: add currentlyLoadingSuggestionIndex property to HomeScreenUiState

--- app/src/main/java/com/example/lookup/ui/home/HomeScreenUiState.kt
+++ app/src/main/java/com/example/lookup/ui/home/HomeScreenUiState.kt
@@ -5,9 +5,12 @@ package com.example.lookup.ui.home
  *  @param identifiedLocation The location identified by the app. A null value indicates
  *  that the app hasn't identified any locations yet.
  *  @param isAnalyzing Indicates whether the current camera stream is being analyzed.
+ *  @param currentlyLoadingSuggestionIndex The index of the currently displayed suggestions
+ *  for which the results are being generated.
  */
 data class HomeScreenUiState(
     val identifiedLocation: IdentifiedLocation? = null,
     val isAnalyzing: Boolean = false,
-    val errorOccurredWhenAnalyzing: Boolean = false
+    val errorOccurredWhenAnalyzing: Boolean = false,
+    val currentlyLoadingSuggestionIndex: Int? = null,
 )
