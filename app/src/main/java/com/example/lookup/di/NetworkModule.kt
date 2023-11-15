c375005511646d0d1dd8ff06f85ad25eaf83dcd4
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-11-14 21:13:26 -0500

    refactor: add di binding for NetworkModule using @Provides

--- app/src/main/java/com/example/lookup/di/NetworkModule.kt
+++ app/src/main/java/com/example/lookup/di/NetworkModule.kt
@@ -1,6 +1,8 @@
 package com.example.lookup.di
 
 import com.example.lookup.BuildConfig
+import com.example.lookup.data.remote.imageclient.ImageClient
+import com.example.lookup.data.remote.imageclient.ImageClientConstants
 import com.example.lookup.data.remote.languagemodels.textgenerator.TextGeneratorClient
 import com.example.lookup.data.remote.languagemodels.textgenerator.TextGeneratorClientConstants
 import dagger.Module
@@ -13,6 +15,28 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import java.net.SocketTimeoutException
 import java.util.concurrent.TimeUnit
 
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object NetworkModule {
+
+    @Provides
+    fun provideTextGeneratorClient(): TextGeneratorClient = Retrofit.Builder()
+        .baseUrl(TextGeneratorClientConstants.BASE_URL)
+        .client(textGeneratorClientOkHttpClient)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+        .create(TextGeneratorClient::class.java)
+
+    @Provides
+    fun provideImageClient(): ImageClient = Retrofit.Builder()
+        .baseUrl(ImageClientConstants.BASE_URL)
+        .client(imageClientOkHttpClient)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+        .create(ImageClient::class.java)
+}
+
 /**
  * The default interceptor for [TextGeneratorClient] used to set the authorization header
  * for each request and also the connect and read timeout.
@@ -21,7 +45,7 @@ import java.util.concurrent.TimeUnit
  * connect and read timeout is not sufficient, and causes an [SocketTimeoutException]. Hence,
  * the default connect timeout and read timeout has to be changed.
  */
-private val lookupAppTextGeneratorClientOkHttpClient by lazy {
+private val textGeneratorClientOkHttpClient by lazy {
     OkHttpClient.Builder()
         .addInterceptor { chain ->
             val newRequest = chain.request().newBuilder()
@@ -34,15 +58,13 @@ private val lookupAppTextGeneratorClientOkHttpClient by lazy {
         .build()
 }
 
-@Module
-@InstallIn(ViewModelComponent::class)
-object NetworkModule {
-
-    @Provides
-    fun provideTextGeneratorClient(): TextGeneratorClient = Retrofit.Builder()
-        .baseUrl(TextGeneratorClientConstants.BASE_URL)
-        .client(lookupAppTextGeneratorClientOkHttpClient)
-        .addConverterFactory(MoshiConverterFactory.create())
+private val imageClientOkHttpClient by lazy {
+    OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            val newRequest = chain.request().newBuilder()
+                .addHeader("Authorization", "Client-ID ${BuildConfig.UNSPLASH_API_ACCESS_KEY}")
+                .build()
+            chain.proceed(newRequest)
+        }
         .build()
-        .create(TextGeneratorClient::class.java)
 }
