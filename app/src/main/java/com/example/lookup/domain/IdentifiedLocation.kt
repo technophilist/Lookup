bfc556594409fa3d8d2a559b5914b97da8b1f2f0
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-11-19 17:53:13 -0500

    refactor: move IdentifiedLocation class to domain package

---
+++ app/src/main/java/com/example/lookup/domain/IdentifiedLocation.kt
@@ -0,0 +1,38 @@
+package com.example.lookup.domain
+
+import com.example.lookup.ui.home.HomeScreen
+
+/**
+ * A data class representing an identified location.
+ *
+ * @property name The name of the location.
+ * @property imageUrls A list of URLs to images of the location.
+ * @property infoCardsContentList A list of [InfoCardContent] objects containing supplementary
+ * information about the location.
+ * @property moreInfoSuggestions A list of suggestions used to give an option to the user to request
+ * more info about a particular location.
+ * @property isBookmarked A boolean indicating whether the location has been bookmarked.
+ */
+data class IdentifiedLocation(
+    val name: String,
+    val imageUrls: List<String>,
+    val infoCardsContentList: List<InfoCardContent>,
+    val moreInfoSuggestions: List<MoreInfoSuggestion>,
+    val isBookmarked: Boolean
+) {
+
+    /**
+     * A data class representing a singular piece of information that will be displayed as a
+     * card in the [HomeScreen].
+     *
+     * @property content The content of the card.
+     */
+    data class InfoCardContent(val content: String)
+
+    /**
+     * A data class representing a suggestion. A suggestion is used to give an option to the user
+     * to request more info about a particular [IdentifiedLocation].
+     */
+    @JvmInline
+    value class MoreInfoSuggestion(val suggestion: String)
+}
--- app/src/main/java/com/example/lookup/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/lookup/ui/home/HomeScreen.kt
@@ -72,45 +72,11 @@ import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.lookup.ui.components.CameraPreview
 import com.example.lookup.R
+import com.example.lookup.domain.IdentifiedLocation
 import com.example.lookup.ui.components.ShutterButton
 import com.example.lookup.ui.utils.BookmarkAdd
 import com.example.lookup.ui.utils.BookmarkAdded
 import com.example.lookup.ui.utils.Bookmarks
-
-/**
- * A data class representing an identified location.
- *
- * @property name The name of the location.
- * @property imageUrls A list of URLs to images of the location.
- * @property infoCardsContentList A list of [InfoCardContent] objects containing supplementary
- * information about the location.
- * @property moreInfoSuggestions A list of suggestions used to give an option to the user to request
- * more info about a particular location.
- * @property isBookmarked A boolean indicating whether the location has been bookmarked.
- */
-data class IdentifiedLocation(
-    val name: String,
-    val imageUrls: List<String>,
-    val infoCardsContentList: List<InfoCardContent>,
-    val moreInfoSuggestions: List<MoreInfoSuggestion>,
-    val isBookmarked: Boolean
-) {
-
-    /**
-     * A data class representing a singular piece of information that will be displayed as a
-     * card in the [HomeScreen].
-     *
-     * @property content The content of the card.
-     */
-    data class InfoCardContent(val content: String)
-
-    /**
-     * A data class representing a suggestion. A suggestion is used to give an option to the user
-     * to request more info about a particular [IdentifiedLocation].
-     */
-    @JvmInline
-    value class MoreInfoSuggestion(val suggestion: String)
-}
 
 @Composable
 fun HomeScreen(
