a88cf9f05681c5ded15e78ef721471a0e90b5c95
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-11-19 18:01:43 -0500

    refactor: rename "message" property to "content" in ConversationMessage domain class

--- app/src/main/java/com/example/lookup/domain/home/ConversationMessage.kt
+++ app/src/main/java/com/example/lookup/domain/home/ConversationMessage.kt
@@ -4,11 +4,11 @@ package com.example.lookup.domain.home
  * Represents a single message in a conversation.
  *
  * @property role The role of the participant who sent the message.
- * @property message The text of the message.
+ * @property content The content of the message.
  */
 data class ConversationMessage(
     val role: Role,
-    val message: String
+    val content: String
 ) {
     /**
      * An enum value that indicates the role of the participant who sent the message.
