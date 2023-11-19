5a2e1380a7be9baa16e54fbefab02f0c8f0349f4
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-11-19 17:52:01 -0500

    refactor: add ConversationMessage domain data class

---
+++ app/src/main/java/com/example/lookup/domain/ConversationMessage.kt
@@ -0,0 +1,27 @@
+package com.example.lookup.domain
+
+/**
+ * Represents a single message in a conversation.
+ *
+ * @property role The role of the participant who sent the message.
+ * @property message The text of the message.
+ */
+data class ConversationMessage(
+    val role: Role,
+    val message: String
+) {
+    /**
+     * An enum value that indicates the role of the participant who sent the message.
+     */
+    enum class Role {
+        /**
+         * Represents an AI assistant.
+         */
+        Assistant,
+
+        /**
+         * Represents the user..
+         */
+        User
+    }
+}
