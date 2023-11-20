6c623332c0b2f44b496689e564eba36d84b34187
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-11-19 19:03:59 -0500

    refactor: add conversationMessages property to HomeScreenUiState

--- app/src/main/java/com/example/lookup/ui/home/HomeScreenUiState.kt
+++ app/src/main/java/com/example/lookup/ui/home/HomeScreenUiState.kt
@@ -1,15 +1,19 @@
 package com.example.lookup.ui.home
 
+import com.example.lookup.domain.home.ConversationMessage
 import com.example.lookup.domain.home.IdentifiedLocation
 
 /**
  * A data class that represents the current UI state of the [HomeScreen].
- *  @param identifiedLocation The location identified by the app. A null value indicates
+ *  @property identifiedLocation The location identified by the app. A null value indicates
  *  that the app hasn't identified any locations yet.
- *  @param isAnalyzing Indicates whether the current camera stream is being analyzed.
+ *  @property conversationMessages A list of [ConversationMessage]s between the user and the
+ *  assistant.
+ *  @property isAnalyzing Indicates whether the current camera stream is being analyzed.
  */
 data class HomeScreenUiState(
     val identifiedLocation: IdentifiedLocation? = null,
+    val conversationMessages: List<ConversationMessage> = emptyList(),
     val isAnalyzing: Boolean = false,
     val errorOccurredWhenAnalyzing: Boolean = false
 )
