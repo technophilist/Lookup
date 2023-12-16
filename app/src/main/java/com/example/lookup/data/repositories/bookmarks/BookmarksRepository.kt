5e56d13a2bb662cb5771c067682747dc66335d8a
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-12-16 12:05:50 -0500

    refactor: add BookmarksRepository interface

---
+++ app/src/main/java/com/example/lookup/data/repositories/bookmarks/BookmarksRepository.kt
@@ -0,0 +1,29 @@
+package com.example.lookup.data.repositories.bookmarks
+
+import com.example.lookup.domain.bookmarks.BookmarkedLocation
+import kotlinx.coroutines.flow.Flow
+
+/**
+ *
+ * A repository for managing saved bookmarks.
+ */
+interface BookmarksRepository {
+    /**
+     * Retrieves a [Flow] of [BookmarkedLocation]s.
+     */
+    suspend fun getBookmarksRepositoryStream(): Flow<List<BookmarkedLocation>>
+
+    /**
+     * Adds a location to the bookmarks.
+     *
+     * @param bookmarkedLocation The location to be added to the bookmarks.
+     */
+    suspend fun addLocationToBookmarks(bookmarkedLocation: BookmarkedLocation)
+
+    /**
+     * Deletes a location from the bookmarks.
+     *
+     * @param bookmarkedLocation The location to be deleted from the bookmarks.
+     */
+    suspend fun deleteLocationFromBookmarks(bookmarkedLocation: BookmarkedLocation)
+}
