e0d0aec83e5ff0ce4baa849c7a8bcd05b9e7bc6e
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-12-16 11:40:11 -0500

    refactor: add BookmarkedLocationEntity to BookmarkedLocation mapper function

--- app/src/main/java/com/example/lookup/domain/bookmarks/BookmarkedLocation.kt
+++ app/src/main/java/com/example/lookup/domain/bookmarks/BookmarkedLocation.kt
@@ -1,8 +1,21 @@
 package com.example.lookup.domain.bookmarks
 
+import com.example.lookup.data.local.bookmarks.BookmarkedLocationEntity
+
 /**
  * A domain class representing a previously bookmarked location by the user.
  * @property name The name of the location.
  * @property imageUrl A [String] containing the url of the associated image.
  */
 data class BookmarkedLocation(val name: String, val imageUrl: String)
+
+
+/**
+ * A mapper function used to map an instance of [BookmarkedLocationEntity] to an instance of
+ * [BookmarkedLocation].
+ */
+fun BookmarkedLocationEntity.toBookmarkedLocation(): BookmarkedLocation = BookmarkedLocation(
+    name = nameOfLocation,
+    imageUrl = imageUrlString
+)
+
