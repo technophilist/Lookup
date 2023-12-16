0ab6cc1fab8a3c2f105163ad6b5c0f22cc39607e
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-12-16 11:35:35 -0500

    refactor: add di provides method for provideBookmarkedLocationsDao

--- app/src/main/java/com/example/lookup/di/DatabaseModule.kt
+++ app/src/main/java/com/example/lookup/di/DatabaseModule.kt
@@ -2,6 +2,8 @@ package com.example.lookup.di
 
 import android.content.Context
 import androidx.room.Room
+import com.example.lookup.data.local.bookmarks.BookmarkedLocationsDao
+import com.example.lookup.data.local.bookmarks.BookmarkedLocationsDatabase
 import com.example.lookup.data.local.cache.landmarks.RecognizedLandmarksDao
 import com.example.lookup.data.local.cache.landmarks.RecognizedLandmarksDatabase
 import dagger.Module
@@ -10,7 +12,8 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 
-private const val DATABASE_FILE_NAME = "recognized_landmarks.db"
+private const val LANDMARKS_DATABASE_FILE_NAME = "recognized_landmarks.db"
+private const val BOOKMARKS_DATABASE_FILE_NAME = "saved_bookmarks.db"
 
 @Module
 @InstallIn(ViewModelComponent::class)
@@ -21,9 +24,17 @@ object DatabaseModule {
         return Room.databaseBuilder(
             context,
             RecognizedLandmarksDatabase::class.java,
-            DATABASE_FILE_NAME
+            LANDMARKS_DATABASE_FILE_NAME
         ).build().getDao()
     }
 
+    @Provides
+    fun provideBookmarkedLocationsDao(@ApplicationContext context: Context): BookmarkedLocationsDao {
+        return Room.databaseBuilder(
+            context,
+            BookmarkedLocationsDatabase::class.java,
+            BOOKMARKS_DATABASE_FILE_NAME
+        ).build().getDao()
+    }
 }
 
