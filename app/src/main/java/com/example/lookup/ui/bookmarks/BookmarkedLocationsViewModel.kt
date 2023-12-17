d2dd6c20022a7e3fa3e81da905ec109065e5b402
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-12-17 17:05:40 -0500

    refactor: add BookmarkedLocationsViewModel

---
+++ app/src/main/java/com/example/lookup/ui/bookmarks/BookmarkedLocationsViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.lookup.ui.bookmarks
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.lookup.data.repositories.bookmarks.BookmarksRepository
+import com.example.lookup.domain.bookmarks.BookmarkedLocation
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class BookmarkedLocationsViewModel @Inject constructor(
+    private val bookmarksRepository: BookmarksRepository
+) : ViewModel() {
+
+    private val _bookmarksListStream = MutableStateFlow<List<BookmarkedLocation>>(emptyList())
+    val bookmarksListStream = _bookmarksListStream as StateFlow<List<BookmarkedLocation>>
+
+
+    init {
+        bookmarksRepository.getBookmarksRepositoryStream().onEach { bookmarkedLocations ->
+            _bookmarksListStream.update { bookmarkedLocations }
+        }.launchIn(viewModelScope)
+    }
+
+    fun deleteBookmark(bookmarkedLocation: BookmarkedLocation) {
+        viewModelScope.launch {
+            bookmarksRepository.deleteLocationFromBookmarks(bookmarkedLocation)
+        }
+    }
+}
