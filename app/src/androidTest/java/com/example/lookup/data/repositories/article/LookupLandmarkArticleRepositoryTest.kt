dddd31adc0bc9aa51862a0c567f5796433d90f7a
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-12-22 13:00:39 -0500

    test: fix missing parameters in LookupLandmarkArticleRepositoryTest

--- app/src/androidTest/java/com/example/lookup/data/repositories/article/LookupLandmarkArticleRepositoryTest.kt
+++ app/src/androidTest/java/com/example/lookup/data/repositories/article/LookupLandmarkArticleRepositoryTest.kt
@@ -1,15 +1,38 @@
 package com.example.lookup.data.repositories.article
 
+import androidx.room.Room
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.lookup.data.local.cache.articles.LandmarkArticleDatabase
 import com.example.lookup.di.NetworkModule
 import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 class LookupLandmarkArticleRepositoryTest {
 
-    private val landmarkArticleRepository = LookupLandmarkArticleRepository(
-        NetworkModule.provideGeminiTextGeneratorClient(),
 
-    )
+    private val context = InstrumentationRegistry.getInstrumentation().targetContext
+    private lateinit var landmarkArticleDatabase: LandmarkArticleDatabase
+    private lateinit var landmarkArticleRepository: LookupLandmarkArticleRepository
+
+    @Before
+    fun setup() {
+        landmarkArticleDatabase = Room.inMemoryDatabaseBuilder(
+            context = context,
+            LandmarkArticleDatabase::class.java
+        ).build()
+        landmarkArticleRepository = LookupLandmarkArticleRepository(
+            textGeneratorClient = NetworkModule.provideGeminiTextGeneratorClient(),
+            landmarkArticleDao = landmarkArticleDatabase.getDao()
+        )
+
+    }
+
+    @After
+    fun tearDown() {
+        landmarkArticleDatabase.close()
+    }
 
     @Test
     fun getArticleTest_validLandmarkDetails_articleIsSuccessfullyFetched() = runTest {
