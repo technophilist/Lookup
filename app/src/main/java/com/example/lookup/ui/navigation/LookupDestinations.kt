c93736e3b282c51645ca3bacbbcfc7d3dd5244c3
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-12-26 17:09:58 -0500

    refactor: add LandmarkDetailScreen to LookupDestinations

--- app/src/main/java/com/example/lookup/ui/navigation/LookupDestinations.kt
+++ app/src/main/java/com/example/lookup/ui/navigation/LookupDestinations.kt
@@ -1,5 +1,7 @@
 package com.example.lookup.ui.navigation
 
+import java.util.Base64
+
 /**
  * A sealed class hierarchy that contains the different destinations of the app.
  * @param route The associated route string of each destination.
@@ -9,5 +11,23 @@ sealed class LookupDestinations(val route: String) {
      * An object representing the home screen.
      */
     object HomeScreen : LookupDestinations("lookup_home_screen")
+
+    /**
+     * An object representing the bookmarks screen.
+     */
     object BookmarksScreen : LookupDestinations("lookup_bookmarks_screen")
+
+    /**
+     * An object representing the landmark details screen.
+     */
+    object LandmarkDetailScreen :
+        LookupDestinations("lookup_landmark_details_screen/{nameOfLandmark}/{encodedImageUrl}") {
+
+        const val NAV_ARG_NAME_OF_LANDMARK = "nameOfLandmark"
+        const val NAV_ARG_ENCODED_IMAGE_URL = "encodedImageUrl"
+        fun buildRoute(nameOfLandmark: String, imageUrl: String): String {
+            val encodedImageUrl = Base64.getUrlEncoder().encodeToString(imageUrl.toByteArray())
+            return "lookup_landmark_details_screen/$nameOfLandmark/$encodedImageUrl"
+        }
+    }
 }
