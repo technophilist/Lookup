10cf9353552c25bcf3bb7d8063212e9379b1db64
technophilist <54663474+technophilist@users.noreply.github.com>
2024-01-02 12:48:11 -0500

    refactor: disable default work manager startup & configure hilt worker factory in Application class

--- app/src/main/java/com/example/lookup/di/LookupApplication.kt
+++ app/src/main/java/com/example/lookup/di/LookupApplication.kt
@@ -1,7 +1,16 @@
 package com.example.lookup.di
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class LookupApplication : Application()
+class LookupApplication : Application(), Configuration.Provider {
+    @Inject
+    lateinit var hiltWorkerFactory: HiltWorkerFactory
+    override val workManagerConfiguration: Configuration = Configuration.Builder()
+        .setWorkerFactory(hiltWorkerFactory)
+        .build()
+}
